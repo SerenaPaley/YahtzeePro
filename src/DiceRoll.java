f4473265990622696759454b4dfb265e21d1d4ff
Serena Paley <spaley@Serenas-MacBook-Pro.local>
2020-09-08 00:21:32 -0500

    Fixed comments and cleaned up code

--- src/DiceRoll.java
+++ src/DiceRoll.java
@@ -1,10 +1,10 @@
 import javax.swing.*;
 import java.awt.*;
 public class DiceRoll extends JComponent {
-
+//creates a dice image based on the randomly generated number
     public static void square(Graphics g, int distance, int num) {
         System.out.println("Trying to print dice");
-        g.drawRect(distance, 50, 100, 100);
+        g.drawRect(distance, 50, 100, 100);     //create a black square
         g.setColor(Color.black);
         g.fillRect(distance, 50, 100, 100);
 
@@ -12,70 +12,65 @@ public class DiceRoll extends JComponent {
         switch (num) {
             //print and fill circles on the squares to look like dice
             case 1:
-                g.drawOval(distance+40, 90, 15, 15);       //dice value of 1
+                g.drawOval(distance+40, 90, 15, 15);       //dice value 1
                 g.setColor(Color.white);
                 g.fillOval(distance+40, 90, 15, 15);
                 break;      //fill
             case 2:
-                g.drawOval(distance+16, 66, 15, 15);       //dice value of 2
-                g.drawOval(distance+65, 115, 15, 15);       //dice value of 2
+                g.drawOval(distance+16, 66, 15, 15);       //dice value 2
+                g.drawOval(distance+65, 115, 15, 15);
                 g.setColor(Color.white);
-                g.fillOval(distance+16, 66, 15, 15);     //fill
+                g.fillOval(distance+16, 66, 15, 15);
                 g.fillOval(distance+65, 115, 15, 15);
                 break;
             case 3:
-                g.drawOval(distance+16, 66, 15, 15);       //dice value of 2
-                g.drawOval(distance+65, 115, 15, 15);       //dice value of 2
-                g.drawOval(distance+40, 90, 15, 15);       //dice value of 2 middle spot
+                g.drawOval(distance+16, 66, 15, 15);       //dice value 3
+                g.drawOval(distance+65, 115, 15, 15);
+                g.drawOval(distance+40, 90, 15, 15);
                 g.setColor(Color.white);
-                g.fillOval(distance+16, 66, 15, 15);     //fill
+                g.fillOval(distance+16, 66, 15, 15);
                 g.fillOval(distance+65, 115, 15, 15);
                 g.fillOval(distance+40, 90, 15, 15);
                 break;
             case 4:
-                g.drawOval(distance+16, 66, 15, 15);       //dice value of 2
-                g.drawOval(distance+16, 115, 15, 15);       //dice value of 2
-                g.drawOval(distance+65, 66, 15, 15);       //dice value of 2 middle spot
-                g.drawOval(distance+65, 115, 15, 15);       //dice value of 2
+                g.drawOval(distance+16, 66, 15, 15);       //dice value 4
+                g.drawOval(distance+16, 115, 15, 15);
+                g.drawOval(distance+65, 66, 15, 15);
+                g.drawOval(distance+65, 115, 15, 15);
                 g.setColor(Color.white);
-                g.fillOval(distance+16, 66, 15, 15);     //fill
+                g.fillOval(distance+16, 66, 15, 15);
                 g.fillOval(distance+16, 115, 15, 15);
                 g.fillOval(distance+65, 66, 15, 15);
                 g.fillOval(distance+65, 115, 15, 15);
                 break;
             case 5:
-                g.drawOval(distance+16, 66, 15, 15);       //dice value of 2
-                g.drawOval(distance+16, 115, 15, 15);       //dice value of 2
-                g.drawOval(distance+65, 66, 15, 15);       //dice value of 2
-                g.drawOval(distance+65, 115, 15, 15);       //dice value of 2
+                g.drawOval(distance+16, 66, 15, 15);       //dice value 5
+                g.drawOval(distance+16, 115, 15, 15);
+                g.drawOval(distance+65, 66, 15, 15);
+                g.drawOval(distance+65, 115, 15, 15);
                 g.drawOval(distance+40, 90, 15, 15);
                 g.setColor(Color.white);
-                g.fillOval(distance+16, 66, 15, 15);     //fill
+                g.fillOval(distance+16, 66, 15, 15);
                 g.fillOval(distance+16, 115, 15, 15);
                 g.fillOval(distance+65, 66, 15, 15);
                 g.fillOval(distance+65, 115, 15, 15);
                 g.fillOval(distance+40, 90, 15, 15);
                 break;
             case 6:
-                g.drawOval(distance+16, 66, 15, 15);       //dice value of 2
-                g.drawOval(distance+16, 90, 15, 15);       //dice value of 2
-                g.drawOval(distance+16, 115, 15, 15);       //dice value of 2
-                g.drawOval(distance+65, 115, 15, 15);       //dice value of 2
+                g.drawOval(distance+16, 66, 15, 15);       //dice value 6
+                g.drawOval(distance+16, 90, 15, 15);
+                g.drawOval(distance+16, 115, 15, 15);
+                g.drawOval(distance+65, 115, 15, 15);
                 g.drawOval(distance+65, 90, 15, 15);
                 g.drawOval(distance+65, 66, 15, 15);
                 g.setColor(Color.white);
-                g.fillOval(distance+16, 66, 15, 15);     //fill
+                g.fillOval(distance+16, 66, 15, 15);
                 g.fillOval(distance+16, 90, 15, 15);
                 g.fillOval(distance+16, 115, 15, 15);
                 g.fillOval(distance+65, 115, 15, 15);
                 g.fillOval(distance+65, 90, 15, 15);
                 g.fillOval(distance+65, 66, 15, 15);
                 break;
-
-
-
-
-
 
         }
 
--- src/YahtzeePro.java
+++ src/YahtzeePro.java
@@ -6,17 +6,9 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.Random;
 
-
-
-
-
 public class YahtzeePro extends JPanel {
 
     static JLabel result = new JLabel("");
-
-
-
-
 
     public static void main(String[] args) {
         JFrame frame = new JFrame("YahtzeePro");
@@ -40,19 +32,9 @@ public class YahtzeePro extends JPanel {
                 System.out.println("It worked");
                 //roll dice and print the result
 
-                //result.setText("You got a " + num);
-                //panel.add(result);
-
-                DiceRoll dice1 = new DiceRoll();
-                DiceRoll dice2 = new DiceRoll();
-                DiceRoll dice3 = new DiceRoll();
-                DiceRoll dice4 = new DiceRoll();
-                DiceRoll dice5 = new DiceRoll();
-                //create an array of dice
-                DiceRoll dice[]= new DiceRoll[5];
+                DiceRoll dice[]= new DiceRoll[5];   //make 5 new dice
                 for(int k=0;k<5;k++)
                 {
-
                    dice[k] = new DiceRoll();
                     panel.add(dice[k]);
                     Random rand = new Random();
@@ -60,51 +42,10 @@ public class YahtzeePro extends JPanel {
                     num = num + 1; // changes number to be between 1-6
                     dice[k].square(panel.getGraphics(), 50+(200*k), num);
 
-                }
+                }//for
+            }//actioned performed
 
-
-
-
-
-
-
-                /* print the result */
-                //JPanel result = new JPanel();
-                //result.setText("You got a " +num);
-                //result.setVisible(true);
-                //result.setSize(500,500);
-                //JTextField result = new JTextField("You got a " +num);
-                //panel.add(result);
-                //JOptionPane.showMessageDialog(null, "You got a " +num);
-
-
-            }
-
-
-        }); {
-        };
-//
-//         class button implements ActionListener{
-//
-//            public void actionPerformed(ActionEvent e) {
-//                //roll dice and print the result
-//                Random rand = new Random();
-//
-//                int num = rand.nextInt(6);   //gives a number between 0-5
-//                num = num + 1; // changes number to be between 1-6
-//                //print the result
-//                //JDialog result = new JDialog(frame, "You got a "+num);
-//
-//
-//                //JLabel result = new JLabel ("You got a " + num);
-//                //JPanel panel = new JPanel();
-//                //fr.add(panel);
-//                //panel.add(result);
-//                //result.setVisible(true);
-//
-//            }
-//        }
-
+        });
 
     }
 
